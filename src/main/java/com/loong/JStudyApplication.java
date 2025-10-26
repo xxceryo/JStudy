fb619804c3e3d3386f955a04578f259df4369d8d
xxceryo <2764733071@qq.com>
2025-10-26 16:43:32 +0800

    第一次提交

---
+++ src/main/java/com/loong/JStudyApplication.java
@@ -0,0 +1,11 @@
+package com.loong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JStudyApplication.class, args);
+    }
+}
